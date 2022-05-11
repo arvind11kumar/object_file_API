bf086d6034d69573a1f8b9a96cd46a85dc2b0aa1
arvind11kumar <71036209+arvind11kumar@users.noreply.github.com>
2022-05-11 10:39:35 +0530

    Add files via upload

---
+++ apiforurl.js
@@ -0,0 +1,19 @@
+const url={weburl:[
+    {id:1,path:"1 https://drive.google.com/file/d/1yZX83vgKnpi5eeaso5fvF8DKGVC1SlfP/view?usp=sharing", name:"copy of react video resource"},
+    {id:2,path:"https://drive.google.com/file/d/1hmlWIlfZAggxkBtX-vNbofE6B_GR9M_P/view?usp=sharing", name:"React Theoretical Resources"},
+    {id:3,path:"https://drive.google.com/drive/folders/1266S__L5gqdg17akrbXoQZhbXUOO6B2h?usp=sharing", name:"project or React"},
+    {id:4,path:" https://drive.google.com/file/d/1vN256zZqnWw84-pncH2qdAFnnlUjtrAy/view?usp=sharing ", name:"Code Challanges React"},
+    {id:5,path:"https://drive.google.com/file/d/1ewVWiyAKLgvn7Ojhp6SDuGW5DYAW18AP/view?usp=sharing", name:"Meta React Material"},
+    {id:6,path:"https://drive.google.com/file/d/1gP9xP0yE-Gj0ZzUA9pgYnRayjAAPGgJt/view?usp=sharing", name:"Js video Resourse"},
+    {id:7,path:"https://drive.google.com/file/d/1mGF5H0S3x37kSsiwS-gK6tPVOg9M5V9I/view?usp=sharing", name:"Js therotical question"},
+    {id:8,path:"https://docs.google.com/document/d/1Ibbi3y1PFv-7TvJbJAowoFvl1bGNb3748Ab0hfAEISU/edit?usp=sharing", name:"JS Implementation resourse"},
+    {id:9,path:"https://drive.google.com/drive/folders/1dz68dE_JQEPYTB-wT1N4RrGzNsOb9vBs?usp=sharing", name:"project"},
+    {id:10,path:"", name:""},
+    {id:11,path:"", name:""},
+    {id:12,path:"", name:""},
+    
+    ]}
+   url.weburl.filter((e)=>{
+       console.table(e)
+       
+   })
---
+++ userdataObject.js
@@ -0,0 +1,25 @@
+const user={newuserdata:[
+    {firstname:"arvind", lastname:"kushwaha",age:21},
+    {firstname:"aruna",  lastname:"chamar",age:52},
+    {firstname:"akash" ,  lastname:"chawhan",age:45},
+    {firstname:"abhinav" ,lastname:"chodan",age:14},
+    {firstname:"anand" ,  lastname:"paswan",age:74},
+    {firstname:"manish"  ,lastname:"devender",age:56},
+    {firstname:"abhinav", lastname:"mahindra",age:85},
+    {firstname:"mohit" ,  lastname:"yadav",age:74},
+    {firstname:"mohan" ,  lastname:"lala",age:56}]};
+
+/////////////////////////////////////////////////////////////////
+////////////////////////how to write data in object formate upper one and down one////////
+///////////////////////////////////////////////////////////////
+
+    const user=[
+        {firstname:"arvind", lastname:"kushwaha",age:21},
+        {firstname:"aruna",  lastname:"chamar",age:52},
+        {firstname:"akash"  , lastname:"chawhan",age:45},
+        {firstname:"abhinav", lastname:"chodan",age:14},
+        {firstname:"anand" ,  lastname:"paswan",age:74},
+        {firstname:"manish" , lastname:"devender",age:56},
+        {firstname:"abhinav", lastname:"mahindra",age:85},
+        {firstname:"mohit" ,  lastname:"yadav",age:74},
+        {firstname:"mohan"  , lastname:"lala",age:56}];
